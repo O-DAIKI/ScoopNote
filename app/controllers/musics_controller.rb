b1b90d97bee7b0f47013f1c2819e96204cf017a0
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-11-19 16:08:19 +0900

    ActiveStorageの導入

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -3,4 +3,12 @@ class MusicsController < ApplicationController
   def index
   end
   
+  def new
+  end
+  
+  private
+
+  def music_params
+    params.require(:music).permit(:image, :file, :artist, :title).merge(user_id: current_user.id)
+  end
 end
