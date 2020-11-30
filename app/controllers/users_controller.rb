4165ef1a5df0dd513e9f95209c710271600d5417
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-11-30 15:31:54 +0900

    ユーザー情報編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,20 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @musics = @user.musics
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to user_path(current_user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:nickname, :profile)
+  end
 end
