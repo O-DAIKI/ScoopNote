750b64f138d6e6f22e59646826ada93125602811
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-12-04 22:42:22 +0900

    ゲストログイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
   root to: "musics#index"
   resources :musics do
     resource :favorites, only: [:create, :destroy]
   end
+
   resources :users, only: [:show, :edit, :update, :favorite] do
     resource :relationships, only: [:create, :destroy]
     get :follows, on: :member
     get :followers, on: :member
+  end
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
 end
