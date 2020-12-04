4d0cad4d7610cb48fe59dfb0d02a3b0977dab639
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-12-04 22:43:56 +0900

    rubocopによる修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    passwords: 'users/passwords'
+    registrations: "users/registrations",
+    passwords: "users/passwords"
   }
   root to: "musics#index"
   resources :musics do
@@ -17,6 +17,6 @@ Rails.application.routes.draw do
   end
 
   devise_scope :user do
-    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+    post "users/guest_sign_in", to: "users/sessions#new_guest"
   end
 end
