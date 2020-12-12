5c7e9027e64c8c13c47597f752641a43a4d9ecf7
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-12-12 20:29:31 +0900

    楽曲機能結合テストコードの実装

--- spec/factories/musics.rb
+++ spec/factories/musics.rb
@@ -2,6 +2,8 @@
 
 FactoryBot.define do
   factory :music do
+    image { "test.jpg" }
+    file { "test.mp3" }
     artist { Faker::Lorem.sentence }
     title { Faker::Lorem.sentence }
     association :user
