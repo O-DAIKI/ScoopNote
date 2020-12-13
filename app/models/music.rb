2ed2362813a517df0817b542602d0fdd0fefce7f
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-12-13 21:59:57 +0900

    ファイルアップローダーのバリデーションの設定

--- app/models/music.rb
+++ app/models/music.rb
@@ -14,6 +14,8 @@ class Music < ApplicationRecord
     validates :title
   end
 
+  validate :image_content_type, if: :image_was_attached?
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
@@ -22,7 +24,7 @@ class Music < ApplicationRecord
     if search != ""
       Music.where("title LIKE(?)", "%#{search}%")
     else
-      Music.all
+      Music.includes(:user).order(created_at: :desc)
     end
   end
 
@@ -32,5 +34,14 @@ class Music < ApplicationRecord
     if !self.image.attached?
       self.image.attach(io: File.open(Rails.root.join("app", "assets", "images", "l_e_others_501.png")), filename: "default-image.png", content_type: "image/png")
     end
+  end
+
+  def image_content_type
+    extension = ['image/png', 'image/jpg', 'image/jpeg']
+    errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
+  end
+
+  def image_was_attached?
+    self.image.attached?
   end
 end
