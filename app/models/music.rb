b74d8c1dd768dcbc1a1336dfba87dea0fc574d2d
O-DAIKI <mesh2.s.m.k@gmail.com>
2020-12-13 22:01:22 +0900

    rubocopによる修正

--- app/models/music.rb
+++ app/models/music.rb
@@ -37,7 +37,7 @@ class Music < ApplicationRecord
   end
 
   def image_content_type
-    extension = ['image/png', 'image/jpg', 'image/jpeg']
+    extension = ["image/png", "image/jpg", "image/jpeg"]
     errors.add(:image, "の拡張子が間違っています") unless image.content_type.in?(extension)
   end
 
